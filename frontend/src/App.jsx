9d9a9385aa7de5097ffb8f0f7c4869a35732957c
Lakshmi Narasimha <lakshminarasimha33333@gmail.com>
2026-04-13 23:20:40 +0530

    changed filename (render) case sensitive

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
-import Home from "./components/Home";
+import Home from "./components/home";
 import AdminDashboard from "./components/AdminDashboard";
 import Auth from "./components/Auth";
 import { useSwiggy } from "./context/SwiggyContext";
 
 const App = () => {
-  const { currentPage, user, seller } = useSwiggy();
+  const { user, seller } = useSwiggy();
 
   // 🔐 Authentication gate
   if (!user?._id && !seller?._id) {
